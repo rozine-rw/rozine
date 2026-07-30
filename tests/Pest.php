7b685d913b84db2672095e05cd17b5f6941fda59
Hussain Aminu <hussain_aminu@yahoo.com>
2026-07-30 08:45:33 +0300

    Fix TIA baseline bootstrap in CI

--- tests/Pest.php
+++ tests/Pest.php
@@ -18,8 +18,6 @@ pest()->extend(TestCase::class)
     ->use(RefreshDatabase::class)
     ->in('Feature');
 
-pest()->tia()->baselined();
-
 /*
 |--------------------------------------------------------------------------
 | Expectations
